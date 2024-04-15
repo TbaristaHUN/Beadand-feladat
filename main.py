3769193a894085d6d5020cbb1d8903b9621d156a
TbaristaHUN <165070325+TbaristaHUN@users.noreply.github.com>
2024-04-15 19:33:42 +0200

    Update main.py

--- main.py
+++ main.py
@@ -98,3 +98,73 @@ while True:
 
     else:
         print("Érvénytelen.")
+from datetime import datetime
+
+class Szoba:
+    def __init__(self, szobaszam, ar):
+        self.szobaszam = szobaszam
+        self.ar = ar
+
+class EgyagyasSzoba(Szoba):
+    def __init__(self, szobaszam):
+        super().__init__(szobaszam, 5000)
+
+class KetagyasSzoba(Szoba):
+    def __init__(self, szobaszam):
+        super().__init__(szobaszam, 8000)
+
+class Foglalas:
+    def __init__(self, szoba, datum):
+        self.szoba = szoba
+        self.datum = datum
+    
+    def __str__(self):
+        return f"Szoba: {self.szoba.szobaszam}, Dátum: {self.datum.strftime('%Y-%m-%d')}"
+
+class Szalloda:
+    def __init__(self, nev):
+        self.nev = nev
+        self.szobak = []
+        self.foglalasok = []
+        self.foglalasok_tombje = []
+
+    def add_szoba(self, szoba):
+        self.szobak.append(szoba)
+
+    def foglalas(self, szobaszam, datum):
+        for szoba in self.szobak:
+            if szoba.szobaszam == szobaszam:
+                foglalas = Foglalas(szoba, datum)
+                self.foglalasok.append(foglalas)
+                self.foglalasok_tombje.append(foglalas)  # Hozzáadás a tömbhöz
+                return foglalas
+        return None
+
+    def lemondas(self, foglalas):
+        if foglalas in self.foglalasok:
+            self.foglalasok.remove(foglalas)
+            return True
+        return False
+
+    def listaz(self):
+        for foglalas in self.foglalasok:
+            print(foglalas)
+
+# Adatok feltöltése
+szalloda = Szalloda("Tibi Szállója")
+szalloda.add_szoba(EgyagyasSzoba("101"))
+szalloda.add_szoba(KetagyasSzoba("201"))
+szalloda.add_szoba(EgyagyasSzoba("102"))
+szalloda.add_szoba(KetagyasSzoba("202"))
+szalloda.add_szoba(EgyagyasSzoba("103"))
+
+szalloda.foglalas("101", datetime(2024, 4, 7))
+szalloda.foglalas("201", datetime(2024, 4, 8))
+szalloda.foglalas("102", datetime(2024, 4, 9))
+szalloda.foglalas("202", datetime(2024, 4, 10))
+szalloda.foglalas("103", datetime(2024, 4, 11))
+
+# Foglalások tömbje
+print("Foglalások tömbje:")
+for foglalas in szalloda.foglalasok_tombje:
+    print(foglalas)
